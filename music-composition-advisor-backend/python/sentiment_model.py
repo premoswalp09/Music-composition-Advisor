8d2029ebdb1112136a7a56750cdbfe4059963034
premoswal <premoswalp09@gmail.com>
2024-11-07 21:38:31 +0530

    Attached frontend and backend, upload model in model folder to run

--- music-composition-advisor-backend/python/sentiment_model.py
+++ music-composition-advisor-backend/python/sentiment_model.py
@@ -1,24 +1,3 @@
-# #This is demo code which needs to be replace by original model inference file.
-
-# import sys
-# import json
-
-# def predict_sentiment(lyrics, genre):
-#     # Placeholder for the model inference
-#     # Load your trained model here and make predictions based on `lyrics` and `genre`
-#     # For now, we'll return a placeholder response
-#     if "love" in lyrics.lower():
-#         return "Positive"
-#     else:
-#         return "Negative"
-
-# if __name__ == "__main__":
-#     lyrics = sys.argv[1]
-#     genre = sys.argv[2]
-#     prediction = predict_sentiment(lyrics, genre)
-#     print(prediction)
-
-
 import sys
 import os
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
